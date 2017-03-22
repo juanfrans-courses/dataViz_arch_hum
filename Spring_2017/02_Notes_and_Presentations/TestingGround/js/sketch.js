060e66de10582523d1f06948af669298f93e2d6d
Juan Francisco Saldarriaga <juanfrans@gmail.com>
2017-03-22 10:59:22 -0400

    update

--- Spring_2017/02_Notes_and_Presentations/TestingGround/js/sketch.js
+++ Spring_2017/02_Notes_and_Presentations/TestingGround/js/sketch.js
@@ -1,30 +1,53 @@
 // ***** Global variables ***** //
-var numberCircles = 20;
-var startY = 50;
-var startX = 50;
-var circleDiameter = 20;
-var distanceBetween = 40;
-var minD = 5;
-var maxD = 50;
+var weatherData;
+var apiKey = '6b7f88ea398f85994ccb7cab38d16545';
+var baseURL = 'http://api.openweathermap.org/data/2.5/weather?q=';
+var city = 'Sydney';
+var units = 'metric';
+var maxTemp = 40;
+var minTemp = -5;
+var maxColor = 360;
+var minColor = 240;
+var currentTemp = 0;
+var currentHumidity = 0;
+var button;
+var cityInput;
 
 // ***** Setup function ***** //
 function setup(){
-    createCanvas(1000, 3000);
+    createCanvas(800, 800);
     colorMode(HSB);
-    textSize(12);
-    console.log('Setup complete...');
+    button = select('#submit');
+    cityInput = select('#city');
+    button.mousePressed(queryAPI);
+}
+
+function queryAPI(){
+  var request = baseURL + cityInput.value() + '&units=' + units + '&apikey=' + apiKey;
+  loadJSON(request, getWeatherData);
+}
+
+function getWeatherData(apiData){
+  weatherData = apiData;
+  currentTemp = weatherData.main.temp;
+  currentHumidity = weatherData.main.humidity;
+  city = cityInput.value();
+  // console.log(weatherData.main.temp);
 }
 
 // ***** Draw function ***** //
 function draw(){
     background(255);
-    stroke(0, 0, 25);
-    strokeWeight(1);
-    for (var i = 0; i < numberCircles; i++) {
-      for (var j = 0; j < numberCircles; j++) {
-        var distnaceToCircle = map(dist(startX + distanceBetween * i, startY + distanceBetween * j, mouseX, mouseY), 0, 1000, minD, maxD);
-        fill(0, 0, map(distnaceToCircle, minD, maxD, 0, 100));
-        ellipse(startX + distanceBetween * i, startY + distanceBetween * j, distnaceToCircle, distnaceToCircle);
-      }
+    if (weatherData) {
+      noStroke();
+      fill(0);
+      text('The current temperature in ' + city + ' is ' + str(currentTemp) + ' C', 20, 20);
+      text('The current humidity in ' + city + ' is ' + str(currentHumidity) + '%', 20, 40);
+      var hueColor = map(currentTemp, minTemp, maxTemp, minColor, maxColor);
+      fill(hueColor, 100, 100);
+      ellipse(100, 100, currentHumidity, currentHumidity);
+    }
+    else{
+      text('Loading...', 20, 20);
     }
 }
