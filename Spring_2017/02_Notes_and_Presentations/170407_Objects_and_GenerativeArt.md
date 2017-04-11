7867975f75593f9c28c6a5ffb1c3656715286341
Juan Francisco Saldarriaga <juanfrans@gmail.com>
2017-04-11 17:42:54 -0400

    update

--- Spring_2017/02_Notes_and_Presentations/170407_Objects_and_GenerativeArt.md
+++ Spring_2017/02_Notes_and_Presentations/170407_Objects_and_GenerativeArt.md
@@ -337,3 +337,54 @@ function Jitter() {
   }
 }
 ```
+
+#### Bouncing balls code
+```js
+// ***** Global variables ***** //
+var nervousBall = []; // Declaring the Array of objects
+var blackWhole;
+
+// ***** Setup function ***** //
+function setup(){
+  createCanvas(800, 800);
+  colorMode(HSB, 360, 100, 100, 100);
+  for (var i = 0; i < 50; i++) {
+    nervousBall.push(new Jitter()); // Create the objects
+  }
+}
+
+// ***** Draw function ***** //
+function draw(){
+  background(255);
+  for (var i = 0; i < nervousBall.length; i++) {
+    nervousBall[i].update();
+    nervousBall[i].display();
+  }
+}
+
+// ***** Jitter class ******* //
+function Jitter() {
+  // Initial properties
+  this.position = createVector(random(100, width - 100), random(100, height - 100));
+  this.diameter = random(10, 50);
+  this.maxVelocity = 10;
+  this.velocity = createVector(random(-this.maxVelocity, this.maxVelocity),random(-this.maxVelocity, this.maxVelocity));
+  this.fill = random(0, 360);
+
+  // Function
+  this.display = function(){
+    stroke(1);
+    fill(this.fill, 100, 100, 75);
+    ellipse(this.position.x, this.position.y, this.diameter, this.diameter);
+  }
+  this.update = function(){
+    if ((this.position.x + this.diameter/2) > width || (this.position.x - this.diameter/2) < 0){
+      this.velocity.x = this.velocity.x * -1;
+    }
+    if ((this.position.y + this.diameter/2) > height || (this.position.y - this.diameter/2) < 0){
+      this.velocity.y = this.velocity.y * -1;
+    }
+    this.position.add(this.velocity);
+  }
+}
+```
--- Spring_2017/02_Notes_and_Presentations/TestingGround/js/sketch.js
+++ Spring_2017/02_Notes_and_Presentations/TestingGround/js/sketch.js
@@ -1,100 +1,47 @@
 // ***** Global variables ***** //
 var nervousBall = []; // Declaring the Array of objects
-var blackWholes = [];
+var blackWhole;
 
 // ***** Setup function ***** //
 function setup(){
   createCanvas(800, 800);
   colorMode(HSB, 360, 100, 100, 100);
-  for (var i = 0; i < 100; i++) {
+  for (var i = 0; i < 50; i++) {
     nervousBall.push(new Jitter()); // Create the objects
-  }
-  for (var i = 0; i < 5; i++) {
-    for (var j = 0; j < 5; j++) {
-      var blackWholesVector = createVector(20 + width / 5 * j, 20 + height / 5 * i);
-      blackWholes.push(new implode(random(0, 1), blackWholesVector));
-    }
   }
 }
 
 // ***** Draw function ***** //
 function draw(){
-  // background(255);
-  fill(0, 0, 100, 5);
-  noStroke();
-  // rect(0, 0, width, height);
+  background(255);
   for (var i = 0; i < nervousBall.length; i++) {
-    nervousBall[i].display();
-    nervousBall[i].calculateDistance();
     nervousBall[i].update();
+    nervousBall[i].display();
   }
-  stroke(0);
-  strokeWeight(1);
-  noFill();
-  // for (var i = 0; i < blackWholes.length; i++) {
-  //   ellipse(blackWholes[i].x, blackWholes[i].y, 100, 100);
-  // }
-}
-
-function implode(gravity, blackWholePosition) {
-  // Initial properties
-  this.position = blackWholePosition;
-  this.gravity = gravity;
 }
 
 // ***** Jitter class ******* //
 function Jitter() {
   // Initial properties
-  this.position = createVector(random(width), random(height));
-  this.diameter = random(1, 2);
-  this.maxVelocity = 1;
+  this.position = createVector(random(100, width - 100), random(100, height - 100));
+  this.diameter = random(10, 50);
+  this.maxVelocity = 10;
   this.velocity = createVector(random(-this.maxVelocity, this.maxVelocity),random(-this.maxVelocity, this.maxVelocity));
-  this.fill = 200;
-  this.maxForce = 5;
-  var gravity = 0.12;
-  var distBlackWhole;
-  var influenceArea = 50;
-  var targetBlackWhole;
-  var inRange = false;
+  this.fill = random(0, 360);
 
   // Function
   this.display = function(){
-    noStroke();
-    fill(this.fill, 100, 100, 100);
+    stroke(1);
+    fill(this.fill, 100, 100, 75);
     ellipse(this.position.x, this.position.y, this.diameter, this.diameter);
   }
-  this.calculateDistance = function(){
-    inRange = false;
-    for (var i = 0; i < blackWholes.length; i++) {
-      distBlackWhole = this.position.dist(blackWholes[i].position);
-      if (distBlackWhole < influenceArea){
-        inRange = true;
-        targetBlackWhole = i;
-      }
-    }
-  }
   this.update = function(){
-    if (inRange){
-      var steerVector = p5.Vector.sub(blackWholes[targetBlackWhole].position, this.position);
-      steerVector.limit(blackWholes[targetBlackWhole].gravity);
-      this.velocity.sub(steerVector);
-      this.velocity.limit(this.maxForce);
-      this.position.add(this.velocity);
+    if ((this.position.x + this.diameter/2) > width || (this.position.x - this.diameter/2) < 0){
+      this.velocity.x = this.velocity.x * -1;
     }
-    else{
-      this.position.add(this.velocity);
+    if ((this.position.y + this.diameter/2) > height || (this.position.y - this.diameter/2) < 0){
+      this.velocity.y = this.velocity.y * -1;
     }
-    if (this.position.x > width){
-      this.position.x = 0;
-    }
-    if (this.position.y > height){
-      this.position.y = 0;
-    }
-    if (this.position.x < 0){
-      this.position.x = width;
-    }
-    if (this.position.y < 0){
-      this.position.y = height;
-    }
+    this.position.add(this.velocity);
   }
 }
